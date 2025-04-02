0bd76b97f9c59a1693db964651ac5767384264e1
Jakub Wasilewski <wasilewskyy@MacBook-Air-Jakub.local>
2025-04-02 14:57:59 +0200

    Initial commit

---
+++ src/main/java/com/wasilewskyy/medical_clinic/MedicalClinicApplication.java
@@ -0,0 +1,13 @@
+package com.wasilewskyy.medical_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalClinicApplication.class, args);
+	}
+
+}
