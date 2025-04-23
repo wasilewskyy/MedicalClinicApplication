e4ab5de089f4016a13d8bbc577c1b0e07b20115e
wasilewskyy <jakubwasilewski84@gmail.com>
2025-04-23 18:03:53 +0200

    fixes

--- src/main/java/com/wasilewskyy/medical_clinic/repository/DoctorRepository.java
+++ src/main/java/com/wasilewskyy/medical_clinic/repository/DoctorRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-    Optional<Doctor> findByName(String name);
+    Optional<Doctor> findByFirstName(String name);
 }
--- src/main/java/com/wasilewskyy/medical_clinic/repository/InstitutionRepository.java
+++ src/main/java/com/wasilewskyy/medical_clinic/repository/InstitutionRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface InstitutionRepository extends JpaRepository<Institution, Long> {
-    Optional<Institution> findByName(String name);
+    Optional<Institution> findByInstitutionName(String name);
 }
